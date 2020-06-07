17ce8e5e41fe458c6839e0261aa021f72037f2d3
Ben Divin <bdivin@sfu.ca>
2020-06-06 17:16:10 -0700

    mostly done

--- public/script.js
+++ public/script.js
@@ -17,6 +17,25 @@ var board = new Array(
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);  // row 16
 
+var marking = new Array(
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // row 0
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);  // row 16
+
 var win = new Array(
         0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, // row 0
         0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
@@ -42,7 +61,7 @@ var message = document.getElementsByClassName("message");
 
 function changeColor(obj,id) {
    if(board[id] == 0){
-     obj.style.backgroundColor = 'blue';
+     obj.style.backgroundColor = '#656565';
      board[id] = 1;
      winCheck();
    }else {
@@ -50,6 +69,16 @@ function changeColor(obj,id) {
      board[id] = 0;
      winCheck();
    }
+}
+
+function changeMark(obj,id) {
+    if(marking[id] == 0){
+        obj.style.backgroundColor = '#FF3333';
+        marking[id] = 1;
+    } else {
+        obj.style.backgroundColor = '';
+        marking[id] = 0;
+    }
 }
 
 function resetBoard(){
@@ -72,11 +101,39 @@ function resetBoard(){
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);  // row 16
 
+  marking = new Array(
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // row 0
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);  // row 16
+
   var i;
-  for(i = 0; i < cells.length-1; i++){
-    cells[i].style.backgroundColor = "";
+  for(i = 0; i <= cells.length-1; i++){
+    cells[i].style.backgroundColor = '';
   }
   message[0].textContent = "Needed to reset huh?";
+}
+
+function markAll(){
+  resetBoard();
+  for(i = 0; i <= cells.length-1; i++){
+      cells[i].style.backgroundColor = '#FF3333';
+      marking[i] = 1;
+  }
+  message[0].textContent = "Why?";
 }
 
 function arraysEqual(a,b){
@@ -99,13 +156,18 @@ function solveBoard(){
   i = win.length;
   count = 0;
   while(i--){
+    if(marking[i] == 1){
+      marking[i] = 0;
+      cells[i].style.backgroundColor = '';
+      count++;
+    }
     if(win[i] != board[i]){
       if(board[i] == 0){
-        board[i] == 1;
-        cells[i].style.backgroundColor = 'blue';
+        board[i] = 1;
+        cells[i].style.backgroundColor = '#656565';
         count++;
       } else if(board[i] == 1){
-        board[i] == 0;
+        board[i] = 0;
         cells[i].style.backgroundColor = '';
         count++;
       }
@@ -116,4 +178,9 @@ function solveBoard(){
   } else {
     message[0].textContent = "Guess you couldn't figure it out huh?";
   }
+}
+
+function showInstructions(){
+  var instructionText = "Left-click on the cells to fill in the correct cells. \n Right-click on the cells to mark off cells that you are certain are not correct. \n The numbers on the sides of the puzzle correspond to how many cells need to be shaded in correctly to solve, as well as the pattern they must be in. \n For example, if the numbers show 1 1 1 then there are 3 squares that need to be filled in and each square is separate from the next square.";
+  message[0].textContent = instructionText;
 }
